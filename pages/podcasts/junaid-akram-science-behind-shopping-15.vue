ae5f30e7fd8eb6fcfef1f7e3acce4c3fe2f3a792
molai.dev <mahir@molai.dev>
2021-07-31 12:08:38 -0700

    first commit

---
+++ pages/podcasts/junaid-akram-science-behind-shopping-15.vue
@@ -0,0 +1,3 @@
+<template>
+    <Podcast title="Science Behind Shopping" speaker="Junaid Akram" source="https://raw.githubusercontent.com/mtgsquad/cdn-content/main/podcasts/Science%20Behind%20Shopping%20_%20Junaid%20Akram's%20Podcast%2315.mp3"/>
+</template>
