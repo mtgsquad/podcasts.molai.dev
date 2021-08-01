2bf195edec88417cec7ef4724943112a68a92a3e
molai.dev <mahir@molai.dev>
2021-08-02 00:07:35 +0500

    newPodcast!

---
+++ pages/podcasts/junaid-akram-secrets-of-israel.vue
@@ -0,0 +1,3 @@
+<template>
+  <Podcast title="The secrets of Israel" speaker="Junaid Akram" source="https://doc-10-3k-docs.googleusercontent.com/docs/securesc/lo8sh27pgoq4gk6klaet726kmphtv02r/7f81ljukkdf9m1j1cmlifsm6l3fa5dm3/1627844700000/12796101457919604104/12796101457919604104/1Nfn3N6DQL2rnB767bG2f1BDXrZpoe0OU?e=download&authuser=0&nonce=ln8tolpnb2pi8&user=12796101457919604104&hash=ajurhhd6fm7tn6lrmqr0nb2er0ks0ib2" lang="Urdu"/>
+</template>
